8710adb1544e57730385db2b2a0267f77b05460d
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-10 18:56:41 +0200

    Add server entry point and configure API routes for posts, users, auth, and profile

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const connectdb = require('./config/db');
+
+const app = express();
+
+// Connect to MongoDB
+connectdb();
+
+app.use('/api', require('./routes/posts'));
+
+app.use('/api', require('./routes/users'));
+
+app.use('/api', require('./routes/auth'));
+
+app.use('/api', require('./routes/profile'));
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+/* 
+    1. We can create all the APIs here in server.js, but it is a good practice to create separate files for each API.
+    We have created separate files for each API in the routes folder.
+    For example:
+    - server/routes/auth.js
+    - server/routes/posts.js
+    - server/routes/users.js
+    - server/routes/profile.js 
+
+    2. The first argument to the app.use() method is the base URL for the API.
+    For example, if you want to create an API for posts, you would use '/api/posts' as the first argument.
+    The second argument is the router object that contains the API endpoints.
+    For example, if you have a route for creating a new post, you would use something like this:
+    app.use('/api/posts', require('./routes/posts'));
+    This will make the API endpoints available at /api/posts/create, /api/posts/get, and so on.
+    The appended URL, for example: /create, is the path defined in the router object.
+*/
