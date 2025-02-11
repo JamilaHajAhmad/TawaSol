68c61c4e6d80d6c216bc87c1421ed5ac3e5c6936
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-11 11:48:36 +0200

    Add Header component to main layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './components/Landing'
+import Header from './components/Header';
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Landing />} />
-  </Routes>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
   </BrowserRouter>
 );
