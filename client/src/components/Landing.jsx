8622e9727888b6358b39592c3db3dbeb191d963a
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-11 11:49:13 +0200

    Add dynamic title rotation to Landing component

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import '../css/Landing.css';
 
 function Landing() {
@@ -7,7 +8,7 @@ function Landing() {
             <div className="overlay">
                 <div className="content">
                     <h1>Welcome to TawaSol</h1>
-                    <p>Your digital solution for a sustainable and environmentally friendly community</p>
+                    <Title />
                     <div className="buttons">
                         <Link to="/register" className="button sign-btn">Sign Up</Link>
                         <Link to="/login" className="button log-btn">Log in</Link>
@@ -16,6 +17,30 @@ function Landing() {
             </div>
         </section>
     );
+}
+
+function Title() {
+    const TITLES = [
+        'Connect, Share, and Engage Effortlessly',
+        'A New Way to Connect with Your Community',
+        'Your Community, Your Voice',
+        'A Platform for Change',
+    ];
+
+    const [titleIndex, setTitleIndex] = useState(0);
+    const [fadeIn, setFadeIn] = useState(true);
+    useEffect(
+        () => {
+            const interval = setInterval(() => {
+                setTitleIndex((titleIndex + 1) % TITLES.length);
+                setFadeIn(true);
+                setTimeout(() => setFadeIn(false), 2000);
+            }, 4000);
+            setTimeout(() => setFadeIn(false), 2000);
+            return () => clearInterval(interval);
+        }
+    , [titleIndex, TITLES.length]);
+    return <p className={`title ${fadeIn? 'title-fade-in': 'title-fade-out'}`}>{TITLES[titleIndex]}</p>
 }
 
 export default Landing;
