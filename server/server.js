a93393b11318d09fdac7ae14a3d187e7183609d3
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-16 10:48:44 +0200

    Add CORS middleware to enable cross-origin requests

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const connectdb = require('./config/db');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 // Connect to MongoDB
 connectdb();
