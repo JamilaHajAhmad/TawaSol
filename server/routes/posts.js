7f5b18c60ca7b99a1fa7624f31eca3b69cd4933f
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-23 11:20:56 +0200

    Add endpoint to create a new post with validation

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const User = require('../models/User');
+const Post = require('../models/Post');
+const { check, validationResult } = require('express-validator');
+const auth = require('../utils');
 const router = express.Router();
 
-router.get('/posts', (req, res) => {
-    res.send('posts route');
+
+// POST /api/posts - Create a new post
+router.post('/',
+    auth,
+    check('text', 'Text is required').notEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                user: req.user.id
+            });
+            const post = await newPost.save();
+            res.json(post);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send(error.message);
+        }
 });
 
 module.exports = router;
