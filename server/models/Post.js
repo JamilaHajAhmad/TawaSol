b5d559a7e8cbef97f12aa093aecc4352c2c33f91
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-23 11:20:34 +0200

    Add Post model with schema for user posts, likes, and comments

---
+++ server/models/Post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Post', postSchema);
+
+/* module.exports = Post = mongoose.model('Post', postSchema);
+   Use the above line if you had any issues with the module.exports line */
