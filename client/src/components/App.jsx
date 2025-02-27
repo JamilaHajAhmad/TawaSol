7ba0719b771fc6fa74f237b36ea67ae01401fb91
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-27 19:38:00 +0200

    Integrate react-toastify for notifications in App component and clean up imports

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Landing from './Landing'
+import Landing from './Landing';
 import Header from './Header';
 import Register from './Users/Register';
 import STORE from '../redux/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Alert from './Alert';
+
 
 const App = () => {
     return (
         <Provider store={STORE}>
             <BrowserRouter>
+                <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} />
+                <Alert />
                 <Header />
                 <Routes>
                     <Route path="/" element={<Landing />} />
@@ -17,6 +23,6 @@ const App = () => {
             </BrowserRouter>
         </Provider>
     );
-}
+};
 
 export default App;
