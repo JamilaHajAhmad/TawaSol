501c38e1c78e5bdd4f14c6eaa18c021fc05107b8
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-28 19:10:04 +0200

    Add country field to ProfileForm component and update state management

--- client/src/components/ProfileForms/ProfileForm.jsx
+++ client/src/components/ProfileForms/ProfileForm.jsx
@@ -2,13 +2,13 @@ import { Fragment, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createProfile, getCurrentProfile, uploadImage } from '../../redux/modules/profiles';
-import PropTypes from 'prop-types';
 import '../../css/ProfileForm.css';
 
 const initialState = {
     company: "",
     website: "",
     location: "",
+    country: "",
     status: "",
     skills: "",
     bio: "",
@@ -31,7 +31,7 @@ const ProfileForm =
             }
         }, [loading, profile, getCurrentProfile]);
 
-        const { company, website, location, status, skills, bio, facebook, linkedin, instagram, github } = formData;
+        const { company, website, location, country, status, skills, bio, facebook, linkedin, instagram, github } = formData;
         const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
         // Without onChange function, the form will not be able to update the state of the input fields.
         // And the form will not be able to recognize that the user is typing in the input fields.
@@ -64,6 +64,7 @@ const ProfileForm =
                     <input type="text" placeholder="Company" name="company" value={company} onChange={onChange} />
                     <input type="text" placeholder="Website" name="website" value={website} onChange={onChange} />
                     <input type="text" placeholder="Location" name="location" value={location} onChange={onChange} />
+                    <input type="text" placeholder="Country" name="country" value={country} onChange={onChange} />
                     <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={onChange} />
                     <input type='textarea' placeholder="A short bio of yourself" name="bio" value={bio} onChange={onChange} />
                     <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button">Social Accounts</button>
@@ -91,13 +92,6 @@ const ProfileForm =
             </div>
         )
 }
-
-ProfileForm.propTypes = {
-    createProfile: PropTypes.func.isRequired,
-    getCurrentProfile: PropTypes.func.isRequired,
-    uploadImage: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired
-};
 
 const mapStateToProps = state => ({
     profiles: state.profiles
