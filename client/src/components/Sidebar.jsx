71f2c1c6de7067e66081df5807e5e14b7bcff378
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-28 17:16:15 +0200

    Add CSS styling to Sidebar links for improved appearance

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
+import '../css/Sidebar.css';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <Link to="/home">Home</Link>
-            <Link to="/posts">Posts</Link>
-            <Link to="/developers">Developers</Link>
-            <Link to="/settings">Settings</Link>
+            <Link className='sidebar-link' to="/home">Home</Link>
+            <Link className='sidebar-link' to="/posts">Posts</Link>
+            <Link className='sidebar-link' to="/developers">Developers</Link>
+            <Link className='sidebar-link' to="/settings">Settings</Link>
         </div>
     );
 }
