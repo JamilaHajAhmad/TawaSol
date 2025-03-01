abe8a19abf25e5a54c538c9e0a931dc3830e7552
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-01 19:11:16 +0200

    Enhance Header component with authentication links and logout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,33 @@
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../redux/modules/users';
 import '../css/Header.css';
-function Header() {
-    return (
-        <header>
-            <img src={logo} alt="TawaSol" className='logo' />
-            <div className="links">
+
+function Header({users: { isAuthenticated }, logout}) {
+    const links = (
+        <div className="links">
                 <Link className='link' to="/">Home</Link>
                 <Link className='link' to="/about">About</Link>
                 <Link className='link' to="/services">Services</Link>
                 <Link className='link' to="/blog">Blog</Link>
-            </div>
+        </div>
+    );
+    const authLinks = (
+        <div className="links">
+            <Link onClick={logout} className='link' to="/">Log out</Link>
+        </div>
+    )
+    return (
+        <header>
+            <img src={logo} alt="TawaSol" className='logo' />
+            <>{isAuthenticated ? authLinks : links}</>
         </header>
     )
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    users: state.users
+});
+
+export default connect(mapStateToProps, { logout})(Header);
