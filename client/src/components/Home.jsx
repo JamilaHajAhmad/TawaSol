a95274b4823bf939165ea42881134de1740f5933
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-01 14:02:07 +0200

    Enhance Home component with Education and Experience sections, including delete functionality

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,27 +1,29 @@
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../redux/modules/profiles';
+import { getCurrentProfile, deleteEducation, deleteExperience } from '../redux/modules/profiles';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Home.css';
 import { getProfileImage } from '../utils';
 import defaultImage from '../assets/default.jpg';
 import BasicInfo from './ProfileInfo/BasicInfo';
+import Education from './ProfileInfo/Education';
+import Experience from './ProfileInfo/Experience';
 
 
-function Home({getCurrentProfile, profiles: { profile }, users: { user }}) {
+function Home({ getCurrentProfile, profiles: { profile }, users: { user } }) {
 
-    const [image, setImage] = useState("");
-    const [error, setError] = useState(false);
-    
+    const [ image, setImage ] = useState("");
+    const [ error, setError ] = useState(false);
+
     useEffect(() => {
         getCurrentProfile();
-        if(user) {
+        if (user) {
             setImage(getProfileImage(user._id))
         }
-    }, [getCurrentProfile, user]);
+    }, [ getCurrentProfile, user ]);
 
     function onError() {
-        if(!error) {
+        if (!error) {
             setError(true);
             setImage(defaultImage);
         }
@@ -36,23 +38,41 @@ function Home({getCurrentProfile, profiles: { profile }, users: { user }}) {
                 </div>
             ) : (
                 <div className="home__profile">
-                    <div className="column">
-                        <img src={image} alt="profile" onError={onError} />
-                        <p>{profile.user.name}</p>
+                    <div className="row">
+                        <div className="column">
+                            <img src={image} alt="profile" onError={onError} />
+                            <p>{profile.user.name}</p>
+                        </div>
+                        <div className="column">
+                            <BasicInfo profile={profile} />
+                            <div className="social">
+                                {profile.social ? Object.keys(profile.social)
+                                    .filter(media => profile.social[ media ] !== "")
+                                    .map(media => {
+                                        return (
+                                            <a key={media} href={profile.social[ media ]} target="_blank" rel="noreferrer">
+                                                <i className={`fab fa-${media} fa-2x`}></i>
+                                            </a>
+                                        )
+                                    })
+                                    : null}
+                            </div>
+                        </div>
                     </div>
-                    <div className="column">
-                        <BasicInfo profile={profile} />
-                        <div className="social">
-                            { profile.social ? Object.keys(profile.social)
-                                                .filter(media => profile.social[media] !== "")
-                                                .map(media => {
-                                                    return (
-                                                        <a key={media} href={profile.social[media]} target="_blank" rel="noreferrer">
-                                                            <i className={`fab fa-${media} fa-2x`}></i>
-                                                        </a>
-                                                    )
-                                                })
-                                : null }
+                    <div className="row">
+                        <div className="column">
+                            <div className="container">
+                                <h3>Education</h3>
+                                <Link to="/add-education"><i className='fas fa-plus-circle fa-2x'></i></Link>
+                            </div>
+                            <Education profile={profile} deleteEducation={deleteEducation} />
+                        </div>
+                        <div className="column">
+                            <div className="container">
+                                <h3>Experience</h3>
+                                <Link to="/add-experience"><i className='fas fa-plus-circle fa-2x'></i></Link>
+                            </div>
+                            <Experience profile={profile} deleteExperience={deleteExperience} />
                         </div>
                     </div>
                 </div>
@@ -73,6 +93,6 @@ const mapStateToProps = (state) => ({
     2. ({}) will give you an implicit return.
 
     3. { profile } will be the object that will be returned from the profiles reducer (in switch case).
-*/ 
+*/
 
 export default connect(mapStateToProps, { getCurrentProfile })(Home);
