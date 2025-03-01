66d9acdc4dccefcd24410117c1053fe2685c0272
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-01 19:12:41 +0200

    Add Settings component for profile management and account deletion

---
+++ client/src/components/Settings.jsx
@@ -0,0 +1,20 @@
+import { connect } from "react-redux";
+import { deleteAccount } from "../redux/modules/profiles";
+import { Link } from "react-router-dom";
+
+const Settings = ({ deleteAccount }) => {
+    return (
+        <div className="settings">
+            <div className="setting-card">
+                <p>To update your profile information, click on the Edit Profile button.</p>
+                <Link to="/edit-profile">Edit Profile</Link>
+            </div>
+            <div className="setting-card">
+                <p>To delete your account, click on the Delete Account button.</p>
+                <button onClick={() => deleteAccount()}>Delete Account</button>
+            </div>
+        </div>
+    )
+}
+
+export default connect(null, { deleteAccount })(Settings);
