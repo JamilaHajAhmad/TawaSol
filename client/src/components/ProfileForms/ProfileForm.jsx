4f7f3de5185c27f113177d1d3e11b842ec47d92f
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-02 09:44:11 +0200

    Refactor ProfileForm component; improve state initialization and enhance social input handling for better profile management

--- client/src/components/ProfileForms/ProfileForm.jsx
+++ client/src/components/ProfileForms/ProfileForm.jsx
@@ -18,80 +18,85 @@ const initialState = {
     github: ""
 }
 
-const ProfileForm =
-    ({ profiles: { profile, loading }, createProfile, getCurrentProfile, uploadImage, history }) => {
-        const [formData, setFormData] = useState({initialState});
-        const [displaySocialInputs, toggleSocialInputs] = useState(false);
+const ProfileForm = ({ profiles: { profile, loading }, createProfile, getCurrentProfile, uploadImage, history }) => {
+    const [formData, setFormData] = useState(initialState);
+    const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
-        useEffect(() => {
-            if (!profile) getCurrentProfile();
-            if (!loading && profile) {
-                const profileData = { ...initialState };
-                setFormData(profileData);
+    useEffect(() => {
+        if (!profile) getCurrentProfile();
+        if (!loading && profile) {
+            const profileData = { ...initialState };
+            for (const key in profile) {
+                if (key in profileData) profileData[key] = profile[key];
             }
-        }, [loading, profile, getCurrentProfile]);
-
-        const { company, website, location, country, status, skills, bio, facebook, linkedin, instagram, github } = formData;
-        const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-        // Without onChange function, the form will not be able to update the state of the input fields.
-        // And the form will not be able to recognize that the user is typing in the input fields.
-        // Not just for the input fields (text, email, password, etc.), but also for the select fields.
-        const onFileChange = e => {
-            const data = new FormData();
-            data.append('file', e.target.files[0]);
-            uploadImage(data);
+            if (profile.social) {
+                for (const key in profile.social) {
+                    if (key in profileData) profileData[key] = profile.social[key];
+                }
+            }
+            setFormData(profileData);
         }
-        const onSubmit = e => {
-            e.preventDefault();
-            createProfile(formData, history, profile ? true : false);
-        };
-        return (
-            <div className="profile-form">
-                <h1>{profile ? 'Edit' : 'Create'} Your Profile</h1>
-                <small>* = required field</small>
-                <form onSubmit={onSubmit}>
-                    <select name="status" value={status} onChange={onChange}>
-                        <option value="0">* Professional Status</option> 
-                        <option value="Developer">Developer</option>
-                        <option value="Junior Developer">Junior Developer</option>
-                        <option value="Senior Developer">Senior Developer</option>
-                        <option value="Manager">Manager</option>
-                        <option value="Student">Student</option>
-                        <option value="Instructor">Instructor</option>
-                        <option value="Intern">Intern</option>
-                        <option value="Other">Other</option>
-                    </select>
-                    <input type="file" name="file" onChange={onFileChange} />
-                    <input type="text" placeholder="Company" name="company" value={company} onChange={onChange} />
-                    <input type="text" placeholder="Website" name="website" value={website} onChange={onChange} />
-                    <input type="text" placeholder="Location" name="location" value={location} onChange={onChange} />
-                    <input type="text" placeholder="Country" name="country" value={country} onChange={onChange} />
-                    <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={onChange} />
-                    <input type='textarea' placeholder="A short bio of yourself" name="bio" value={bio} onChange={onChange} />
-                    <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button">Social Accounts</button>
-                    {displaySocialInputs ? (
-                        <Fragment>
-                            <div>
-                                <i className="fab fa-facebook fa-x"/><input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={onChange} />
-                            </div>
-                            <div>
-                                <i className="fab fa-linkedin fa-x"/><input type="text" placeholder="LinkedIn URL" name="linkedin" value={linkedin} onChange={onChange} />
-                            </div>
-                            <div>
+    }, [loading, profile, getCurrentProfile]);
+
+    const { company, website, location, country, status, skills, bio, facebook, linkedin, instagram, github } = formData;
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onFileChange = e => {
+        const data = new FormData();
+        data.append('file', e.target.files[0]);
+        uploadImage(data);
+    }
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history, profile ? true : false);
+    };
+
+    return (
+        <div className="profile-form">
+            <h1>{profile ? 'Edit' : 'Create'} Your Profile</h1>
+            <small>* = required field</small>
+            <form onSubmit={onSubmit}>
+                <select name="status" value={status} onChange={onChange}>
+                    <option value="0">* Professional Status</option> 
+                    <option value="Developer">Developer</option>
+                    <option value="Junior Developer">Junior Developer</option>
+                    <option value="Senior Developer">Senior Developer</option>
+                    <option value="Manager">Manager</option>
+                    <option value="Student">Student</option>
+                    <option value="Instructor">Instructor</option>
+                    <option value="Intern">Intern</option>
+                    <option value="Other">Other</option>
+                </select>
+                <input type="file" name="file" onChange={onFileChange} />
+                <input type="text" placeholder="Company" name="company" value={company} onChange={onChange} />
+                <input type="text" placeholder="Website" name="website" value={website} onChange={onChange} />
+                <input type="text" placeholder="Location" name="location" value={location} onChange={onChange} />
+                <input type="text" placeholder="Country" name="country" value={country} onChange={onChange} />
+                <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={onChange} />
+                <input type='textarea' placeholder="A short bio of yourself" name="bio" value={bio} onChange={onChange} />
+                <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button">Social Accounts</button>
+                {displaySocialInputs ? (
+                    <Fragment>
+                        <div>
+                            <i className="fab fa-facebook fa-x"/><input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={onChange} />
+                        </div>
+                        <div>
+                            <i className="fab fa-linkedin fa-x"/><input type="text" placeholder="LinkedIn URL" name="linkedin" value={linkedin} onChange={onChange} />
+                        </div>
+                        <div>
                             <i className="fab fa-instagram fa-x"/><input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={onChange} />
-                            </div>
-                            <div>
-                                <i className="fab fa-github fa-x"/><input type="text" placeholder="Github URL" name="github" value={github} onChange={onChange} />
-                            </div>
-                        </Fragment>
-                    ) : <Fragment />}
-                    <div>
-                        <input type="submit" value={profile ? 'Update Profile' : 'Create Profile'} />
-                        <Link to="/home" className='link'>Go Back</Link>
-                    </div>
-                </form>
-            </div>
-        )
+                        </div>
+                        <div>
+                            <i className="fab fa-github fa-x"/><input type="text" placeholder="Github URL" name="github" value={github} onChange={onChange} />
+                        </div>
+                    </Fragment>
+                ) : <Fragment />}
+                <div>
+                    <input type="submit" value={profile ? 'Update Profile' : 'Create Profile'} />
+                    <Link to="/home" className='link'>Go Back</Link>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 const mapStateToProps = state => ({
