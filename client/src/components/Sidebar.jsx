3345d688f1b2a9d016995751b82f99a54cab9041
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-02 15:23:54 +0200

    Add Sidebar component; implement profile image display with error handling and connect to Redux for user profile

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,36 @@
 import { Link } from 'react-router-dom';
 import '../css/Sidebar.css';
+import { useState, useEffect } from 'react';
+import { getProfileImage } from '../utils';
+import { getCurrentProfile } from '../redux/modules/profiles';
+import { connect } from 'react-redux';
+import defaultImage from '../assets/default.jpg';
 
-const Sidebar = () => {
+const Sidebar = ({ getCurrentProfile, users: { user } }) => {
+    const [ image, setImage ] = useState("");
+    const [ error, setError ] = useState(false);
+
+    useEffect(() => {
+        getCurrentProfile();
+        if (user) {
+            setImage(getProfileImage(user._id))
+        }
+    }, [ getCurrentProfile, user ]);
+
+    function onError() {
+        if (!error) {
+            setError(true);
+            setImage(defaultImage);
+        }
+    }
+
     return (
         <div className="sidebar">
+            <div className="image">
+                <Link to="/home">
+                    <img src={image} alt="profile" onError={onError} />
+                </Link>
+            </div>
             <Link className='sidebar-link' to="/home">Home</Link>
             <Link className='sidebar-link' to="/posts">Posts</Link>
             <Link className='sidebar-link' to="/developers">Developers</Link>
@@ -12,4 +39,6 @@ const Sidebar = () => {
     );
 }
 
-export default Sidebar;
+const mapStateToProps = state => ({ users: state.users });
+
+export default connect(mapStateToProps, { getCurrentProfile })(Sidebar);
