3cff6aeb361d029540aebe1b59d8ef126175a21f
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-02 09:24:42 +0200

    Add styling to Settings component; enhance Edit Profile link with a button class for improved UI

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,13 +1,14 @@
 import { connect } from "react-redux";
 import { deleteAccount } from "../redux/modules/profiles";
 import { Link } from "react-router-dom";
+import '../css/Settings.css';
 
 const Settings = ({ deleteAccount }) => {
     return (
         <div className="settings">
             <div className="setting-card">
                 <p>To update your profile information, click on the Edit Profile button.</p>
-                <Link to="/edit-profile">Edit Profile</Link>
+                <Link to="/edit-profile" className="edit-profile-button">Edit Profile</Link>
             </div>
             <div className="setting-card">
                 <p>To delete your account, click on the Delete Account button.</p>
