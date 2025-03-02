cad7b8a20b947d64544f2312ec90f5763d3563ff
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-02 09:09:41 +0200

    Add Experience component styling and improve layout; implement delete icon visibility

--- client/src/components/ProfileInfo/Experience.jsx
+++ client/src/components/ProfileInfo/Experience.jsx
@@ -1,16 +1,21 @@
 import { formatDate } from "../../utils";
+import '../../css/Experience.css';
 
 const Experience = ({ profile, deleteExperience }) => {
     return (
-        <div className="container">
+        <div className="experience-container">
             {profile.experience.map(exp => (
                 <div key={exp._id} className="experience">
-                    {deleteExperience ? <i className='fas fa-trash-alt fa-2x' onClick={deleteExperience(exp._id)}></i> : null}
-                    <p>{exp.current ? 'Working' : 'Worked'} as <b>{exp.title}</b> at <b>{exp.company}</b> {exp.location ? <span>at <b>{exp.location}</b></span> : null}</p>
+                    {deleteExperience ? <i className='fas fa-trash-alt fa-x trash-icon' onClick={() => deleteExperience(exp._id)}></i> : null}
+                    <p>&#128188; {exp.current ? 'Working' : 'Worked'} as <b>{exp.title}</b> at <b>{exp.company}</b> {exp.location ? <span>at <b>{exp.location}</b></span> : null}</p>
                     <small>from {formatDate(exp.from)} to {exp.to ? formatDate(exp.to) : 'Now'}</small>
                 </div>
             ))}
-            {profile.experience.length === 0 ? <p>No experience added</p> : null}
+            {profile.experience.length === 0 ? (
+                <div className="experience">
+                    <p>No experience added</p>
+                </div>
+            ) : null}
         </div>
     )
 
