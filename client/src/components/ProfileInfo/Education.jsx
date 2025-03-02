29262565eaa7bc275a2e4bf44cadc6a421e9bf18
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-02 20:59:23 +0200

    Update trash icon styling in Education and Experience components

--- client/src/components/ProfileInfo/Education.jsx
+++ client/src/components/ProfileInfo/Education.jsx
@@ -7,7 +7,7 @@ const Education = ({profile, deleteEducation}) => {
             {profile.education.map(edu => (
                 <div key={edu._id} className="education">
                     {deleteEducation !== undefined ?
-                        <a href="#" onClick={() => deleteEducation(edu._id)}><i className='fas fa-trash-alt fa-x trash-icon'/></a> : null}
+                        <a href="#" onClick={() => deleteEducation(edu._id)}><i className='fas fa-trash trash-icon'/></a> : null}
                     <p>&#x1F393; {edu.current ? 'Studying' : 'Studied'} <b>{edu.degree}</b> of <b>{edu.fieldofstudy}</b> at <b>{edu.school}</b></p>
                     <small>from {formatDate(edu.from)} to {edu.to ? formatDate(edu.to) : 'Now'}</small>
                 </div>
--- client/src/components/ProfileInfo/Experience.jsx
+++ client/src/components/ProfileInfo/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = ({ profile, deleteExperience }) => {
         <div className="experience-container">
             {profile.experience.map(exp => (
                 <div key={exp._id} className="experience">
-                    {deleteExperience ? <i className='fas fa-trash-alt fa-x trash-icon' onClick={() => deleteExperience(exp._id)}></i> : null}
+                    {deleteExperience ? <i className='fas fa-trash trash-icon' onClick={() => deleteExperience(exp._id)}></i> : null}
                     <p>&#128188; {exp.current ? 'Working' : 'Worked'} as <b>{exp.title}</b> at <b>{exp.company}</b> {exp.location ? <span>at <b>{exp.location}</b></span> : null}</p>
                     <small>from {formatDate(exp.from)} to {exp.to ? formatDate(exp.to) : 'Now'}</small>
                 </div>
